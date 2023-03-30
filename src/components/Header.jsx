7d7f139ed5084dabf5dad5cd9b3d8c51a6ba7741
ankitgond123 <ankitgond9566@gmail.com>
2023-03-30 14:28:19 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+
+export default class Header extends Component{
+	
+render = () => {
+return(
+<div>
+<a href="#home">Home</a>|
+<a href="#createuser">User</a>|
+<a href="#showusers">Show User</a>
+
+</div>
+
+)
+}
+}
